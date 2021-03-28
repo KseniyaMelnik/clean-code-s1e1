753031df6cb194fbadc3ec1d2737ff54e7720b74
Kseniya <kseniyamelnnik@gmail.com>
2021-03-28 15:05:31 +0300

    refactor: rename selectors in css according to rule 3.1

--- app.js
+++ app.js
@@ -37,16 +37,20 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="task-checkbox";
+
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task task-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="btn edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="delete-img";
     deleteButton.appendChild(deleteButtonImg);
 
+    listItem.className="task-item";
 
     //and appending.
     listItem.appendChild(checkBox);
